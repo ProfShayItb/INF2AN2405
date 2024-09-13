3388151434cd9f2145916127f1b66dad4ab72923
Sharlayne Angel Jardini Fonseca <sharlayne.fonseca@docente.fieb.edu.br>
2024-09-13 18:09:01 -0300

    atualização

--- src/components/Primeiro/Primeiro.jsx
+++ src/components/Primeiro/Primeiro.jsx
@@ -5,6 +5,7 @@ export default function Primeiro() {
     return (
         <div>
             <h1>Primeiro Componente</h1>
+            <p>Esse é o primeiro componente!</p>
         </div>
     )
 }
